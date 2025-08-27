cdb0467e740b37fdae47b3385b73d11e545e3a53
Himel <shahriarhasan221@gmail.com>
2025-08-27 09:59:09 +0600

    added alerts

--- home.js
+++ home.js
@@ -1,3 +1,6 @@
+const validPin = 1234;
+const validBankAccountNumber = "12345678901";
+
 document
   .getElementById("add-money-btn")
   .addEventListener("click", function (e) {
@@ -7,10 +10,26 @@ document
       "bank-account-number"
     ).value;
     const amountToAdd = parseInt(document.getElementById("add-amount").value);
-    const pinNumber = document.getElementById("pin-number").value;
+    const pinNumber = parseInt(document.getElementById("pin-number").value);
     const availableBalance = parseInt(
       document.getElementById("available-balance").innerText
     );
+    if (bank === "Select Bank") {
+      alert("Please select a bank");
+      return;
+    }
+    if (bankAccountNumber !== validBankAccountNumber) {
+      alert("Please enter the correct 11-digit bank account number");
+      return;
+    }
+    if (isNaN(amountToAdd) || amountToAdd <= 0) {
+      alert("Please enter a valid amount to add");
+      return;
+    }
+    if (pinNumber !== validPin) {
+      alert("Please enter the correct 4-digit pin number");
+      return;
+    }
     const newBalance = availableBalance + amountToAdd;
     document.getElementById("available-balance").innerText = newBalance;
   });
